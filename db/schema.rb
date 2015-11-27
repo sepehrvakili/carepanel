951d4c83d2b235ed937e550ca9c1ca95d15f6bb7
heysep <sdvakili@gmail.com>
2015-11-26 22:30:55 -0500

    Migrate hospital model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151127032442) do
+ActiveRecord::Schema.define(version: 20151127033005) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,16 @@ ActiveRecord::Schema.define(version: 20151127032442) do
     t.integer  "hospital_id"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+  end
+
+  create_table "hospitals", force: :cascade do |t|
+    t.string   "name"
+    t.string   "address"
+    t.string   "city"
+    t.string   "state"
+    t.string   "phone"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "patients", force: :cascade do |t|
---
+++ spec/factories/hospitals.rb
@@ -0,0 +1,10 @@
+FactoryGirl.define do
+  factory :hospital do
+    name "MyString"
+address "MyString"
+city "MyString"
+state "MyString"
+phone "MyString"
+  end
+
+end
