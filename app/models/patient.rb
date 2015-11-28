9a35373f4228c61309e4398815ea700ef403522a
heysep <sdvakili@gmail.com>
2015-11-28 18:12:15 -0500

    Add paperclip avatar field and validations to model

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,4 +1,11 @@
 class Patient < ActiveRecord::Base
+
 	has_many :caretakers
 	has_many :physicians, :through => :caretakers
+
+	validates :name, :room, :avatar, presence: true
+
+	has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }
+	validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
+
 end
